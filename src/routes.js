8fd142c00fa35d7b6eb30ac94a524e954c433f64
ChadDiaz <chadcdiaz@gmail.com>
2021-09-15 20:51:07 -0400

    added src folder with server and routes js files

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const appRouter = express.Router();
+
+appRouter.get("/*", function (req, res) {
+  res.send("<h1>Hello, World</h1>");
+});
+
+module.exports = appRouter;
